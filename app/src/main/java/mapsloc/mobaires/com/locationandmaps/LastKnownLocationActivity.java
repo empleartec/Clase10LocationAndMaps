14cbfe9e8ffd4361343652ca6a207d951a927d9d
Cristian Duarte <cristian.duarte.mb@gmail.com>
2016-01-21 21:47:26 -0300

    Trying get last known location - (NOT WORKING)

--- app/src/main/java/mapsloc/mobaires/com/locationandmaps/LastKnownLocationActivity.java
+++ app/src/main/java/mapsloc/mobaires/com/locationandmaps/LastKnownLocationActivity.java
@@ -1,21 +1,123 @@
 package mapsloc.mobaires.com.locationandmaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-public class LastKnownLocationActivity extends AppCompatActivity {
+public class LastKnownLocationActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    protected static final String TAG = "LastKnownLocationActivity";
+    protected static final String TAG = "LKLActivity";
+
+    /**
+     * Punto de entrada a Google Play Services.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Ultima location.
+     */
+    protected Location mLastLocation;
+
+    private TextView mLatitudeTextView;
+    private TextView mLongitudeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last_known_location);
+
+        buildGoogleApiClient();
+
+        mLatitudeTextView = (TextView) findViewById(R.id.latitudeTextView);
+        mLongitudeTextView = (TextView) findViewById(R.id.longitudeTextView);
     }
 
+
+    /**
+     * Construye GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    public static int REQUEST_COARSE_LOCATION;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        int permissionStatus = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (/*ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            && */permissionStatus
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        getAndShowLocation();
+    }
+
+    public void getAndShowLocation() {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeTextView.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeTextView.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (REQUEST_COARSE_LOCATION==requestCode) {
+            // We have requested multiple permissions for contacts, so all of them need to be
+            // checked.
+            if (PermissionUtil.verifyPermissions(grantResults)) {
+                // All required permissions have been granted, display contacts fragment.
+                getAndShowLocation();
+            } else {
+                Log.i(TAG, "Contacts permissions were NOT granted.");
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
